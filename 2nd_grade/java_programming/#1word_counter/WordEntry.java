24de212f9fe16d13862b74ed98f5486b88557fa2
mki <145424645+melnikovknst@users.noreply.github.com>
2025-03-04 13:39:05 +0700

    Update WordEntry.java

--- 2nd_grade/java_programming/#1word_counter/WordEntry.java
+++ 2nd_grade/java_programming/#1word_counter/WordEntry.java
@@ -1,16 +1,16 @@
 import java.util.Objects;
 
 public class WordEntry {
-    private String word;
+    private final String word;
     private int count;
 
     public WordEntry(String word) {
         this.word = word;
-        this.count = 1;
+        this.count = 0;
     }
 
     public void incrementCount() {
-        this.count++;
+        count++;
     }
 
     public String getWord() {
@@ -23,14 +23,23 @@ public class WordEntry {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
         WordEntry wordEntry = (WordEntry) obj;
+
         return Objects.equals(word, wordEntry.word);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(word);
+    }
+
+    @Override
+    public String toString() {
+        return word + ": " + count;
     }
 }
