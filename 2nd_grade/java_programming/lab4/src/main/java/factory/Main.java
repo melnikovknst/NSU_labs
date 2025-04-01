f388dbb12264ed739f75588b4a6053111f079b5f
mki <145424645+melnikovknst@users.noreply.github.com>
2025-04-01 16:03:01 +0700

    Update Main.java

--- 2nd_grade/java_programming/lab4/src/main/java/factory/Main.java
+++ 2nd_grade/java_programming/lab4/src/main/java/factory/Main.java
@@ -10,9 +10,9 @@ import factory.dealers.Dealer;
 import factory.gui.FactoryGUI;
 
 import javax.swing.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.ArrayList;
 
 public class Main
 {
@@ -29,13 +29,13 @@ public class Main
         int supplierMotorDelay = Integer.parseInt(config.getProperty("supplier_motor_delay"));
         int supplierAccessoryDelay = Integer.parseInt(config.getProperty("supplier_accessory_delay"));
         int dealerDelay = Integer.parseInt(config.getProperty("dealer_delay"));
-        int workers = Integer.parseInt(config.getProperty("workers"));
+        int threadPoolSize = Integer.parseInt(config.getProperty("workers"));
         int dealerCount = Integer.parseInt(config.getProperty("dealer_count"));
 
-        Storage<Body> bodyStorage = new Storage<>(bodyStorageCapacity, "Body Storage");
-        Storage<Motor> motorStorage = new Storage<>(motorStorageCapacity, "Motor Storage");
-        Storage<Accessory> accessoryStorage = new Storage<>(accessoryStorageCapacity, "Accessory Storage");
-        Storage<Car> carStorage = new Storage<>(carStorageCapacity, "Car Storage");
+        Storage<Body> bodyStorage = new Storage<>(bodyStorageCapacity);
+        Storage<Motor> motorStorage = new Storage<>(motorStorageCapacity);
+        Storage<Accessory> accessoryStorage = new Storage<>(accessoryStorageCapacity);
+        Storage<Car> carStorage = new Storage<>(carStorageCapacity);
 
         Supplier<Body> bodySupplier = new Supplier<>(bodyStorage, Body.class, supplierBodyDelay);
         Supplier<Motor> motorSupplier = new Supplier<>(motorStorage, Motor.class, supplierMotorDelay);
@@ -49,27 +49,24 @@ public class Main
         motorSupplierThread.start();
         accessorySupplierThread.start();
 
-        ThreadPool threadPool = new ThreadPool(workers);
+        ThreadPool threadPool = new ThreadPool(threadPoolSize);
 
         FactoryController factoryController = new FactoryController(carStorage, bodyStorage, motorStorage, accessoryStorage, threadPool);
         Thread factoryControllerThread = new Thread(factoryController);
         factoryControllerThread.start();
 
         List<Dealer> dealers = new ArrayList<>();
-        List<Thread> dealerThreads = new ArrayList<>();
-        for (int i = 1; i <= dealerCount; i++)
+        for (int i = 0; i < dealerCount; i++)
         {
-            Dealer dealer = new Dealer(carStorage, i, dealerDelay, true, factoryController);
-            dealers.add(dealer);
+            dealers.add(new Dealer(carStorage, i + 1, dealerDelay, true, factoryController));
+        }
+
+        for (Dealer dealer : dealers)
+        {
             Thread dealerThread = new Thread(dealer);
-            dealerThreads.add(dealerThread);
             dealerThread.start();
         }
 
-        SwingUtilities.invokeLater(() -> new FactoryGUI(
-                bodyStorage, motorStorage, accessoryStorage, carStorage,
-                bodySupplier, motorSupplier, accessorySupplier,
-                dealers, threadPool, factoryController
-        ).setVisible(true));
+        SwingUtilities.invokeLater(() -> new FactoryGUI(bodyStorage, motorStorage, accessoryStorage, carStorage, bodySupplier, motorSupplier, accessorySupplier, dealers, threadPool, factoryController).setVisible(true));
     }
 }
